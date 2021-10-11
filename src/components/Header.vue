95cc91a799f31a9a527df67b83c396f49897fc39
Hemil78 <zanattahemil@gmail.com>
2021-10-11 23:18:53 +0200

    header and hero

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,105 @@
 <template>
     <header>
+        <div class="header-up">
 
+            <div class=" box-header-up container">
+                <div class="call">
+                     <a href="#"><i class="fas fa-phone"></i> Call us for a Free Quote 1.800.555.6789</a>                                               
+                </div>
+                <div class="icons-header">
+                    <ul>
+                        <li v-for="(icon, index) in icons" :key="index">
+                            <a :href="icon.url"><i :class="icon.name"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+                    
+        <div class="header-down">
+            <div class="box-header-down container">
+
+                <div class="logo-header">
+                    <img src="../images/avada-movers-logo.png" alt="logo-movers">
+                </div>
+
+                <div class="nav-header">
+                    <div class="nav">
+                        <ul>
+                            <li v-for="(link, index) in menu" :key="index">
+                                <a :href="link.url" :class="{active : link.current}">{{link.text}}</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button>free quote</button>
+                </div>
+
+            </div>
+
+        </div>
     </header>
 </template>
 
 <script>
 export default {
   name: 'Header',
-  props: {
-    
+  data() {
+    return {
+        icons: [
+            {
+                name: 'fab fa-facebook-f',
+                url: '#',
+                current: true
+            },
+            {
+                name: 'fab fa-twitter',
+                url: '#',
+                current: false
+            },
+            {
+                name: 'fab fa-instagram',
+                url: '#',
+                current: false
+            },
+            {
+                name: 'fab fa-youtube',
+                url: '#',
+                current: false
+            }
+        ],
+        menu: [
+            {
+                text: 'Home',
+                url: '#',
+                current: true
+            },
+            {
+                text: 'Rates',
+                url: '#',
+                current: false
+            },
+            {
+                text: 'Testimonials',
+                url: '#',
+                current: false
+            },
+            {
+                text: 'FAQ',
+                url: '#',
+                current: false
+            },
+            {
+                text: 'Blog',
+                url: '#',
+                current: false
+            },
+            {
+                text: 'Contact',
+                url: '#',
+                current: false
+            }
+        ]
+    }
   }
 }
 </script>
@@ -17,5 +108,99 @@ export default {
 <style scoped lang="scss">
 
 @import '../css/commons.scss';
+
+.header-up{
+    width: 100%;
+    height: 45px;
+    background-color: $colorPrimary;
+
+    .box-header-up{
+        height: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 0;
+
+        .call{
+
+            a{
+                color: #fff;
+                text-decoration: none;
+                font-size: 14px;
+
+                i{
+                    margin-right: 10px;
+                }
+            } 
+        }
+        .icons-header{
+            display: flex;
+
+            ul{
+                list-style: none;
+                
+                li{
+                    display: inline-block;
+                    
+                    a{
+                        color: #fff;
+                        margin-left: 25px;
+                    }
+                }
+            }
+        }
+    }
+}
+.header-down{
+    width: 100%;
+    background-color: transparent;
+    position: absolute;
+    top: 45px;
+    z-index: 999;
+
+    .box-header-down{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 15px 0;
+
+        .logo-header{
+            display: flex;
+            align-items: center;
+        }
+
+        .nav-header{
+            display: flex;
+
+            .nav{
+                display: flex;
+                align-items: center;
+                ul{
+                    list-style: none;
+                    li{
+                        display: inline-block;
+                        
+                        a{
+                            text-decoration: none;
+                            margin-left: 40px;
+                            font-weight: bold;
+                            color: #000;
+
+                            &.active{
+                                color: $colorPrimary;
+                            }
+                        }
+                    }
+                }
+            }
+            button{
+                @include btn;
+                margin-left: 40px;
+                cursor: pointer;
+            }
+        }
+    }
+}
+
 
 </style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="hero-box-container">
+        <div class="hero-box">
+            <div class="hero-text">
+                <h4>moving without the hassle</h4>
+                <h1>we make it simple</h1>
+                <h4>professional service with quality and customer satisfaction</h4>
+            </div>
+        </div>
+        <div class="get">
+            <a href="#"><i class="far fa-paper-plane"></i>get a free quote online now!</a>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Hero'
+}
+</script>
+
+<style scoped lang="scss">
+
+@import '../css/commons.scss';
+.hero-box-container{
+    width: 100%;
+        .hero-box{
+        width: 100%;
+        min-height: 590px;
+        background-image: url(../images/avada-movers-homeherobackground-final.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        .hero-text {
+
+            text-align: center;
+            margin-top: 80px;
+
+            h4{
+                @include textPrimary;
+            }
+            h1{
+                text-transform: uppercase;
+                color: #000;
+                font-size: 65px;
+                font-weight: bold;
+                padding: 15px 0;
+            }
+        }
+        
+    }
+    .get{
+        width: 100%;
+        background-color: $colorSecondary;
+        @include positionElm;
+
+        a{
+            text-decoration: none;
+            color:#fff;
+            text-transform: uppercase;
+            letter-spacing: 5px;
+            padding: 60px 0;
+            transition: .3s;
+            &:hover{
+                font-weight: bold;
+            }
+
+            i{
+                margin-right: 10px;
+                font-size: 20px;
+                transition: .3s;
+            }
+        }
+    }
+}
+
+
+</style>
